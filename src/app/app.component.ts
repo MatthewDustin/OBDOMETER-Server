673dc9727130343f8c2afe2bfa5274edcd1377a6
matthewdustin <dustinmj@appstate.edu>
2023-04-01 19:19:24 -0400

    working on passwords

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as jsonData from '../assets/passwords.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'apphack';
+  data: any = jsonData;
+
+  ngOnInit() {
+    console.log('Data', this.data);
+  }
 }
 
 
