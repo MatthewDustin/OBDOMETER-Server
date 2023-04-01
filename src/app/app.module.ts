3c14f203475b536f4ea14f47a9d5b56f902c9132
matthewdustin <dustinmj@appstate.edu>
2023-04-01 17:15:56 -0400

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DownloadComponent } from './download/download.component';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
