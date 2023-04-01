880d76355c0d53d33976609b7df67da51deabb61
matthewdustin <dustinmj@appstate.edu>
2023-04-01 17:27:42 -0400

    added formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -19,7 +19,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     AppRoutingModule,
     NoopAnimationsModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
