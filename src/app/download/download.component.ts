c3af4f05d5058bd4cacd0a42f0d1073f1cc7e0e3
matthewdustin <dustinmj@appstate.edu>
2023-04-01 21:41:23 -0400

    download button

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -8,4 +8,45 @@ import { ngxCsv } from 'ngx-csv/ngx-csv';
 })
 export class DownloadComponent {
 
+  getCSV() {
+    ngxCsv(this.data, this.filename, this.options);
+  }
+  options = {
+    fieldSeparator: ',',
+    quoteStrings: '"',
+    decimalseparator: '.',
+    showLabels: false,
+    showTitle: false,
+    title: 'Your title',
+    useBom: true,
+    noDownload: false
+  };
+
+  filename = "data.csv"
+
+  data = [
+    {
+      name: "Test 1",
+      age: 13,
+      average: 8.2,
+      approved: true,
+      description: "using 'Content here, content here' "
+    },
+    {
+      name: 'Test 2',
+      age: 11,
+      average: 8.2,
+      approved: true,
+      description: "using 'Content here, content here' "
+    },
+    {
+      name: 'Test 4',
+      age: 10,
+      average: 8.2,
+      approved: true,
+      description: "using 'Content here, content here' "
+    },
+  ];
+
+
 }
