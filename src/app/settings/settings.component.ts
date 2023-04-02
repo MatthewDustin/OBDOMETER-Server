9fe5e0aa1ee1658e8413b191e9e84cae20bfbec1
matthewdustin <dustinmj@appstate.edu>
2023-04-01 21:58:15 -0400

    rerouting

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    let loggedIn = sessionStorage.getItem("loggedIn");
+    if (loggedIn != "true") {
+      this.router.navigate(['login']);
+    }
+  }
 
+  onSubmit(form: any) { }
 }
